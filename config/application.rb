ec715b660c142b8c608c1ddbfaf12a5927c32da9
Keisuke Yoshida <y.keisuke0220@gmail.com>
2015-09-02 11:30:16 +0900

    Update config/application.rb for Heroku

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,8 @@ module CbMorning
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # for heroku deploy
-    config.assets.initialize_on_precompile = false
+    
+  config.assets.initialize_on_precompile = false
   end
 
   
