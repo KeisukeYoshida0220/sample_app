3dca4eb5bbcb7025521c50935ff0ff60d9ebd34a
Keisuke Yoshida <y.keisuke0220@gmail.com>
2015-09-02 11:12:25 +0900

    Add precompiled assets for Heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ Bundler.require(:default, Rails.env)
 
 module CbMorning
   class Application < Rails::Application
+
+    #config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -26,6 +28,6 @@ module CbMorning
     # config.i18n.default_locale = :de
   end
 
-  config.assets.initialize_on_precompile = false
+  
 
 end
