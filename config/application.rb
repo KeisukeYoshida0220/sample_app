ffb3797330e7af1907d4ebae458579a2d7adbcd6
Keisuke Yoshida <y.keisuke0220@gmail.com>
2015-09-02 15:21:24 +0900

    Initial commit

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
-module CbMorning
+module SampleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -24,10 +24,5 @@ module CbMorning
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
-  config.assets.initialize_on_precompile = false
   end
-
-  
-
 end
